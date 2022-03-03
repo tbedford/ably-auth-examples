dbca93257350c2a1fb7a575c72f92e1d02479785
Tony Bedford <tony.bedford@live.co.uk>
2022-03-03 11:06:26 +0000

    Adds authParams usage

--- client-authurl.js
+++ client-authurl.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const Ably = require("ably");
-const auth_url = "https://"+process.env.HOSTNAME+"/auth";
-const auth_headers = {'User-Agent': 'IoT client v0.2'}
-const ably = new Ably.Realtime({authUrl: auth_url, authHeaders: auth_headers});
+const auth_url = "https://" + process.env.HOSTNAME + "/auth";
+const auth_headers = { "User-Agent": "IoT client v0.2" };
+
+const ably = new Ably.Realtime({
+  authUrl: auth_url,
+  authParams: { username: process.env.USERNAME, password: process.env.PASSWORD },
+  authHeaders: auth_headers,
+});
 
 ably.connection.on("connecting", () => {
   console.log("Connecting to Ably...");
